5be2e382d86f25df5ba9f095f3213a4c8a255cca
Stole1234 <novakstosic2003@gmail.com>
2024-11-24 15:24:08 +0100

    dodato dugme za gledano-ne gledano i dodata opcija za rejting filma

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { MovieService } from '../movie/movie.service'; 
-
+import { MovieService } from '../movie/movie.service';
 
 @Component({
   selector: 'app-cart',
@@ -10,45 +9,46 @@ import { MovieService } from '../movie/movie.service';
 export class CartComponent implements OnInit {
   reservedMovies: any[] = [];
 
-  constructor(private movieService: MovieService) {} 
+  constructor(private movieService: MovieService) {}
 
   ngOnInit() {
+    // Ispravite ključ sa kojim pretrazujete localStorage
     const storedProjections = localStorage.getItem('reservedProjections');
     if (storedProjections) {
-      this.reservedMovies = JSON.parse(storedProjections); 
+      this.reservedMovies = JSON.parse(storedProjections);
     }
   }
-  
+
   getMovieById(id: number) {
-    
     return this.movieService.getMovieById(id);
   }
 
   removeMovie(movie: any) {
-    this.reservedMovies = this.reservedMovies.filter(
-      (m) =>
-        m.movieTitle !== movie.movieTitle ||
-        m.date !== movie.date ||
-        m.time !== movie.time
-    );
-  
+    // Ispravite uslov za filtriranje filma na osnovu jedinstvenog ID-a
+    this.reservedMovies = this.reservedMovies.filter(m => m.id !== movie.id);
+    
+    // Ažurirajte localStorage sa novim nizom rezervacija
     localStorage.setItem('reservedProjections', JSON.stringify(this.reservedMovies));
     alert('Rezervacija je otkazana.');
   }
-  
 
   clearCart() {
+    // Čisti sve rezervacije iz korpe
     this.reservedMovies = [];
-    localStorage.removeItem('reservedMovies');
+    localStorage.removeItem('reservedProjections');
   }
 
   rateProjection(movie: any) {
     const index = this.reservedMovies.findIndex((m) => m.id === movie.id);
     if (index !== -1) {
       this.reservedMovies[index].rating = movie.rating;
-      localStorage.setItem('reservedMovies', JSON.stringify(this.reservedMovies));
+      localStorage.setItem('reservedProjections', JSON.stringify(this.reservedMovies));
     }
   }
-  
-  
+
+  toggleMovieStatus(movie: any): void {
+    // Promeni status filma
+    movie.status = movie.status === 'gledano' ? 'ne gledano' : 'gledano';
+    localStorage.setItem('reservedProjections', JSON.stringify(this.reservedMovies));
+  }
 }
